1c480a199ad9420cf5c9b01bcfea85c680cedc3a
Reichenbach, Michael <755327+Silthus@users.noreply.github.com>
2026-09-22 05:56:06 +0200

    Revert "feat(workflows): wait three days before the welcome email"
    This reverts commit 96115840e4cb9f1167fa2ace2241235ba00c48fe.

--- products/workflows/workflows/welcome-new-signups.ts
+++ products/workflows/workflows/welcome-new-signups.ts
@@ -17,7 +17,7 @@ export const welcomeNewSignups = workflow({
     status: 'draft',
     on: onEvent({ event: 'user signed up' }),
     steps: path(
-        delay('3d', { name: 'Wait three days' }),
+        delay('1d', { name: 'Wait a day' }),
         branch({
             name: 'Has an email address?',
             branches: [{ name: 'Has an email address', when: [person('email', 'is_set')], then: path(welcomeEmail) }],
